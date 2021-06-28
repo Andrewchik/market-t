a82fae98e14f360de906836fd7849ca1ad2d5801
illya <barna.illya@gmail.com>
2021-06-28 14:40:55 +0300

    added new custom error

--- src/helpers/error.helper.js
+++ src/helpers/error.helper.js
@@ -1,11 +1,18 @@
 import { toast } from "react-toastify";
 
-import { NO_ITEM_WITH_THAT_ID, ITEM_IS_NOT_AVAILABLE_FOR_PURCHASE } from "../constants";
+import {
+    NO_ITEM_WITH_THAT_ID,
+    ITEM_IS_NOT_AVAILABLE_FOR_PURCHASE,
+    NOT_ENOUGH_BALANCE
+} from "../constants";
 
 //TODO: add more custom errors here
 export const showErrorMessage = (e) => {
     if (e && e.toString().includes(NO_ITEM_WITH_THAT_ID))
         return toast.error(ITEM_IS_NOT_AVAILABLE_FOR_PURCHASE);
+
+    if (e && e.toString().includes(NOT_ENOUGH_BALANCE))
+        return toast.error(NOT_ENOUGH_BALANCE);
 
     toast.error(e);
 }
