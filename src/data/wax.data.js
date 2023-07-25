d824126cef06c80bc96fb0872e0808da6b84bd99
Andrewchik <andriy.goryachiy16@gmail.com>
2023-07-25 21:29:41 +0300

    [UPD] medium link

--- src/data/wax.data.js
+++ src/data/wax.data.js
@@ -3,5 +3,5 @@ import IMXDropBanner from "../images/banner/immutable_drop_banner.png";
 
 export const banners = [
     {id: 1, topTitle: 'NOW LIVE', title: 'SDM Market', text: 'Now you can buy, sell and trade Dark Country assets, using SDM, the official token of the Dark Country game.', btnText: 'VIEW', image: SDMBanner, link: '/market?blockchain=WAX'},
-    {id: 2, topTitle: 'UPCOMING', title: 'Cursed Standard Pack ImmutableX Drop', text: 'Join the adventure of the Dark Country Cursed Collection, featuring 40 handcrafted, highly detailed artworks.', btnText: 'READ MORE', image: IMXDropBanner, link: 'https://darkcountrygame.medium.com/cursed-collection-on-immutablex-early-access-discount-for-sale-8eb045c23317'},
+    {id: 2, topTitle: 'UPCOMING', title: 'Cursed Standard Pack ImmutableX Drop', text: 'Join the adventure of the Dark Country Cursed Collection, featuring 40 handcrafted, highly detailed artworks.', btnText: 'READ MORE', image: IMXDropBanner, link: 'https://darkcountrygame.medium.com/haunting-cursed-collection-is-coming-to-immutablex-e1d6d68fba23'},
 ]
